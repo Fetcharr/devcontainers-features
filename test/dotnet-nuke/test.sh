2e7c6ccf20ae9ab15d9e9285af4c058022c0f177
Max T. Kristiansen <me@maxtrier.dk>
2024-07-31 15:16:31 +0200

    fix: Fix test failing on Ubuntu

--- test/dotnet-nuke/test.sh
+++ test/dotnet-nuke/test.sh
@@ -4,8 +4,6 @@
 set -e
 source dev-container-features-test-lib
 
-# "nuke :Complete -Root" is one of the only command that doesn't require an existing `.nuke` folder.
-# https://github.com/nuke-build/nuke/blob/6ed9e4d9c7476444eb6b984591d1364b47723144/source/Nuke.GlobalTool/Program.Complete.cs#L24
-check "Check NUKE version" nuke :Complete -Root
+check "Check NUKE" test "$(dotnet tool list --global | grep nuke.globaltool | wc -l)" = "1"
 
 reportResults
