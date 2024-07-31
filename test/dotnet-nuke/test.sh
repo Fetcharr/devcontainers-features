a46856109e5600f5ef395f6f525228c93582a83c
Max T. Kristiansen <me@maxtrier.dk>
2024-07-31 15:39:54 +0200

    ci: Fix CI again

--- test/dotnet-nuke/test.sh
+++ test/dotnet-nuke/test.sh
@@ -4,6 +4,6 @@
 set -e
 source dev-container-features-test-lib
 
-check "Check NUKE" test "$(dotnet tool list --global | grep nuke.globaltool | wc -l)" = "1"
+check "Check NUKE" test "$(dotnet tool list --global | grep nuke.globaltool | echo $?)" = "0"
 
 reportResults
